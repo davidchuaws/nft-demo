8a7ba96a4092b62eaed4f3e417d9dd3f536a1ff9
davidchuaws <35304739+davidchuaws@users.noreply.github.com>
2022-04-16 01:08:15 +0800

    Initial commit

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v09.6/VRFConsumerBase.sol";
+
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyHash;
+    uint256 public fee;
+    enum Animal {MONKEY, TIGER, CROCODILE, DUMBO, REINDEER, ELEPHANT, DONKEY, BEAR, CRICKET, CAT, UNICORN, HUSKY, OCTOPUS, BABY-SEAL, OWL, SEAL, POLAR-BEAR, OTTER, PANDA, PUG}
+    mapping(uint256 => Animal) public tokenIdToAnimal;
+    mapping(bytes32 => address) public requestIdToSender;
+
+    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyHash, uint256 fee) 
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+        ERC721("ZooAnimal", "ZOO")
+    {
+        tokenCounter = 0;
+        keyHash = _keyHash;
+        fee = _fee;
+    }
+
+    function createCollectible() public returns (bytes32) {
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+        Animal animal = Animal(randomNumber % 20);
+        uint256 newTokenId = tokenCounter;
+        tokenIdToAnimal[newTokenId] = animal;
+        address owner = requestIdToSender[requestId]
+        _safeMint(owner, newTokenId);
+        tokenCounter = tokenCounter + 1;
+    }
+
+    function 
+}
