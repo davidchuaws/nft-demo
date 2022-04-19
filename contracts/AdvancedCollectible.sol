148a642ff3ea820e66558a4abfa376310cfb899c
davidchuaws <35304739+davidchuaws@users.noreply.github.com>
2022-04-20 01:05:28 +0800

    Added unit and integration tests
    Modified img names as well

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -3,17 +3,19 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v09.6/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
     uint256 public tokenCounter;
     bytes32 public keyHash;
     uint256 public fee;
-    enum Animal {MONKEY, TIGER, CROCODILE, DUMBO, REINDEER, ELEPHANT, DONKEY, BEAR, CRICKET, CAT, UNICORN, HUSKY, OCTOPUS, BABY-SEAL, OWL, SEAL, POLAR-BEAR, OTTER, PANDA, PUG}
+    enum Animal {MONKEY, TIGER, CROCODILE, DUMBO, REINDEER, ELEPHANT, DONKEY, BEAR, CRICKET, CAT, UNICORN, HUSKY, OCTOPUS, BABYSEAL, OWL, SEAL, POLARBEAR, OTTER, PANDA, PUG}
     mapping(uint256 => Animal) public tokenIdToAnimal;
     mapping(bytes32 => address) public requestIdToSender;
+    event requestedCollectible(bytes32 indexed requestId, address requester);
+    event animalAssigned(uint256 indexed tokenId, Animal animal);
 
-    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyHash, uint256 fee) 
+    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyHash, uint256 _fee) public
         VRFConsumerBase(_vrfCoordinator, _linkToken)
         ERC721("ZooAnimal", "ZOO")
     {
@@ -25,18 +27,21 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     function createCollectible() public returns (bytes32) {
         bytes32 requestId = requestRandomness(keyHash, fee);
         requestIdToSender[requestId] = msg.sender;
+        emit requestedCollectible(requestId, msg.sender);
     }
 
     function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
         Animal animal = Animal(randomNumber % 20);
         uint256 newTokenId = tokenCounter;
         tokenIdToAnimal[newTokenId] = animal;
-        address owner = requestIdToSender[requestId]
+        emit animalAssigned(newTokenId, animal);
+        address owner = requestIdToSender[requestId];
         _safeMint(owner, newTokenId);
         tokenCounter = tokenCounter + 1;
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
-        
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not owner nor approved!");
+        _setTokenURI(tokenId, _tokenURI);
     }
 }
---
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -0,0 +1,33 @@
+from web3 import Web3
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
+from brownie import AdvancedCollectible, network, config
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+
+def main():
+    deploy_and_create()
+
+def deploy_and_create():
+    account = get_account()
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from" : account}
+    )
+    fund_with_link(advanced_collectible.address)
+    creating_tx = advanced_collectible.createCollectible({"from": account})
+    creating_tx.wait(1)
+    print("New token has been created!")
+    return advanced_collectible, creating_tx
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=Web3.toWei(0.3, "ether")
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    funding_tx = link_token.transfer(contract_address, amount, {"from": account})
+    funding_tx.wait(1)
+    print(f"Funded {contract_address}")
+    return funding_tx
